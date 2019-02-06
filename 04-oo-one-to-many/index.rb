b349b425cf2c59695a488bae0342b3a68f67dd7a
MinimumViablePerson <nicolas.marcora@yahoo.co.uk>
2019-02-06 12:18:54 +0000

    intro to sql

--- 04-oo-one-to-many/index.rb
+++ 04-oo-one-to-many/index.rb
@@ -1,8 +1,1 @@
 # Object Oriented Ruby - One to Many
-
-
-#   * `#tweets` that returns an array of Tweet instances
-#   * `#post_tweet` that takes a message, creates a new tweet, and adds it to the user's tweet collection
-
-#   * `Tweet.all` that returns all the Tweets created.
-#   * `Tweet#username` that returns the username of the tweet's user
